0fe46e78a727c543a55d54103fa132ed7a49896a
ryaaners <ryaanmahmood@gmail.com>
2025-05-24 16:51:26 -0400

    Blackjack.java

--- src/Blackjack.java
+++ src/Blackjack.java
@@ -1,15 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Blackjack {
+
+    public static void main(String[] args) throws Exception {
+
+        BlackjackGame mygame = new BlackjackGame();
+
+        mygame.initializeGame();
+        do {
+            mygame.shuffle();
+            mygame.getBets();
+            mygame.dealCards();
+            mygame.printStatus();
+            mygame.checkBlackjack();
+            mygame.hitOrStand();
+            mygame.dealerPlays();
+            mygame.settleBets();
+            mygame.printMoney();
+            mygame.clearHands();
+        } while (mygame.playAgain());
+        //mygame.endGame();
+
+        try
+        {
+            FileOutputStream outObjectFile = new FileOutputStream("objOut", false);
+
+            ObjectOutputStream outObjectStream = new ObjectOutputStream(outObjectFile);
+
+            outObjectStream.writeObject(mygame);
+
+            outObjectStream.flush();
+            outObjectStream.close();
+        }
+        catch(FileNotFoundException fnfException)
+        {
+            System.out.println("No file");
+        }
+        catch(IOException ioException)
+        {
+            System.out.println("bad IO");
+        }
+
+        try
+        {
+            FileInputStream inObjectFile = new FileInputStream("objOut");
+
+            ObjectInputStream inObjectStream = new ObjectInputStream(inObjectFile);
+
+            Card myNewCard = (Card)inObjectStream.readObject();
+
+            System.out.println(myNewCard);
+
+        }
+        catch(FileNotFoundException fnfException)
+        {
+            System.out.println("No File");
+        }
+        catch(IOException ioException)
+        {
+            System.out.println("IO no good");
+        }
+
+        catch(ClassNotFoundException cnfException)
+        {
+            System.out.println("This is not a Card.");
         }
     }
 }
