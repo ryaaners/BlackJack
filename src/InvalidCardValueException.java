a86588255c463e335f7bdf0775534393b5eaaecc
ryaaners <ryaanmahmood@gmail.com>
2025-05-24 16:54:14 -0400

    InvalidCardValueException.java

--- src/InvalidCardValueException.java
+++ src/InvalidCardValueException.java
@@ -1,7 +1,28 @@
-package PACKAGE_NAME;
+public class InvalidCardValueException extends Exception
+{
+  private int valueIdentifier = 0;
 
-public class InvalidCardValueException extends RuntimeException {
-  public InvalidCardValueException(String message) {
-    super(message);
+  public InvalidCardValueException(int invalidValue) {
+
+    valueIdentifier = invalidValue;
+
+    System.out.println("Invalid value " + invalidValue);
+  }
+
+  private InvalidCardValueException() {
+
+
+    System.out.println("Invalid value");
+  }
+
+  public String toString() {
+
+
+    return ("Attempted to create card with invalid suit argument" + " " + this.valueIdentifier);
+  }
+
+  public int getValue() {
+
+    return valueIdentifier;
   }
 }
